c4fe510a0d25761413c550f418dcd7701889e2a8
BM Hudson <ttnhan@brickmate.vn>
2022-04-21 18:11:08 +0700

    wip

--- lib/screens/restaurant_details/restaurant_details_screen.dart
+++ lib/screens/restaurant_details/restaurant_details_screen.dart
@@ -48,7 +48,8 @@ class RestaurantDetailScreen extends StatelessWidget {
               height: 250,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.vertical(
-                  bottom: Radius.elliptical(MediaQuery.of(context).size.width, 50),
+                  bottom:
+                      Radius.elliptical(MediaQuery.of(context).size.width, 50),
                 ),
                 image: DecorationImage(
                   fit: BoxFit.cover,
@@ -57,9 +58,80 @@ class RestaurantDetailScreen extends StatelessWidget {
               ),
             ),
             RestaurantInformation(restaurant: restaurant),
+            ListView.builder(
+              padding: EdgeInsets.zero,
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: restaurant.tags.length,
+              itemBuilder: (context, index) {
+                return _buildMenuItems(restaurant, context, index);
+              },
+            )
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildMenuItems(
+      Restaurant restaurant, BuildContext context, int index) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10,
+          ),
+          child: Text(
+            restaurant.tags[index],
+            style: Theme.of(context)
+                .textTheme
+                .headline3!
+                .copyWith(color: Theme.of(context).colorScheme.secondary),
+          ),
+        ),
+        Column(
+          children: restaurant.menuItems
+              .where((menuItem) => menuItem.category == restaurant.tags[index])
+              .map(
+                (e) => Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: ListTile(
+                        dense: true,
+                        contentPadding: EdgeInsets.zero,
+                        title: Text(
+                          e.name,
+                          style: Theme.of(context).textTheme.headline5,
+                        ),
+                        subtitle: Text(
+                          e.description,
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                        trailing: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text('\$${e.price}'),
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.add_circle,
+                                color: Theme.of(context).colorScheme.secondary,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+              .toList(),
+        ),
+      ],
     );
   }
 }
