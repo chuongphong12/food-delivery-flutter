65f09d07ffc7ef2bbdf227645da3b5780799df9c
BM Hudson <ttnhan@brickmate.vn>
2022-04-26 18:06:51 +0700

    wip

--- lib/screens/filter/filter_screen.dart
+++ lib/screens/filter/filter_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:food_delivery_app_flutter/models/category_model.dart';
-import 'package:food_delivery_app_flutter/models/price_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:food_delivery_app_flutter/blocs/filters/filters_bloc.dart';
 import 'package:food_delivery_app_flutter/widgets/custom_category_filter.dart';
 import 'package:food_delivery_app_flutter/widgets/custom_price_filter.dart';
 
@@ -20,6 +20,42 @@ class FilterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Filter')),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BlocBuilder<FiltersBloc, FiltersState>(
+              builder: (context, state) {
+                if (state is FilterLoading) {
+                  return const Center(
+                    child: CircularProgressIndicator.adaptive(),
+                  );
+                }
+                if (state is FiltersLoaded) {
+                  return ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Theme.of(context).primaryColor,
+                      shape: const RoundedRectangleBorder(),
+                      padding: const EdgeInsets.symmetric(horizontal: 50),
+                    ),
+                    onPressed: () {
+                      print(state.filter.categoryFilters;
+                      var categories = state.filter.categoryFilters
+                          .where((filter) => filter.value)
+                          .map((filter) => filter.category.name)
+                          .toList();
+                      print(categories);
+                    },
+                    child: const Text('Apply'),
+                  );
+                } else {
+                  return const Text('Something went wrong');
+                }
+              },
+            ),
+          ],
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Column(
@@ -38,7 +74,7 @@ class FilterScreen extends StatelessWidget {
                     color: Theme.of(context).colorScheme.secondary,
                   ),
             ),
-            const CustomCategoryFilter()
+            const CustomCategoryFilter(),
           ],
         ),
       ),
